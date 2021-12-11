c4a2ecb4bb1fbc9792b177882e2b09d2fc11b65e
cccrizzz <ccccrizzzz@gmail.com>
2021-12-11 01:44:08 -0500

    escrow contract

---
+++ contracts/Escrow.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+
+contract Escrow is AccessControl{
+
+    // roles
+    bytes32 public constant Agent = keccak256("AGENT");
+    bytes32 public constant Sender = keccak256("SENDER");
+    bytes32 public constant Receiver = keccak256("RECEIVER");
+
+    // time limit
+    uint256 timeLimit = 1 minutes;
+
+    // contract deploy time
+    uint256 deployTime;
+
+    // receiver => amount mapping
+    mapping(address => uint256) receiver_amount;
+
+
+
+    constructor(){
+        // set deployTime
+        deployTime = block.timestamp;
+
+        // set agent role
+        _grantRole(Agent, msg.sender);
+
+    }
+
+
+    // deposite function
+    function DepositeFor(address receiver) public payable{
+        // set the role of sender 
+        _grantRole(Sender, msg.sender);
+
+
+        // set role of receiver
+        _grantRole(Receiver, receiver);
+
+        // set mappings
+        receiver_amount[receiver] = msg.value;
+
+    }
+
+
+
+    // can be either called by sender or receiver
+    function Withdraw() public {
+        // time differnce
+        uint256 deltaTime = block.timestamp - deployTime;
+
+        if (hasRole(Sender, msg.sender)) {
+            // if sender called
+            require(address(this).balance > 0, "no funds available");
+            require(deltaTime >= timeLimit, "you cant get refund before time limit");
+            payable(msg.sender).transfer(address(this).balance);
+        } else if (hasRole(Receiver, msg.sender)){
+            // if receiver called
+            require(deltaTime < timeLimit, "time is up, the funds are returned to the sender");
+            payable(msg.sender).transfer(receiver_amount[msg.sender]);
+        }
+
+    }
+
+
+
+    function getBalance() public view returns(uint256) {
+        return address(this).balance;
+    }
+
+
+}
